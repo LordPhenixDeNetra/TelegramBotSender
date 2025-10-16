57c12582093b214520529d90592d8bed4848bd20
MOUSSA THIOR <netrathior@gmail.com>
2025-10-16 23:40:18 +0200

    Update Config System

---
+++ config.exemple.py
@@ -0,0 +1,14 @@
+# Configuration du bot Telegram
+
+# Token du bot
+BOT_TOKEN = 'votre_token_ici'
+
+# Configuration de la base de données
+DB_FILE = 'bootsender.db'
+
+# Liste initiale des destinataires (sera importée dans la base de données)
+INITIAL_USERS = {
+    'USER Y': '00000000000', # Remplacez par le chat_id de USER Y
+    'USER X': '00000000000', # Remplacez par le chat_id de USER X
+}
+
